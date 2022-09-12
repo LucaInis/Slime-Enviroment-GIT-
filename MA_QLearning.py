cfeb5282cf4a5f7f668c1c2e82965cb24f5f6bcf
Stefano Mariani, PhD <stefano.mariani@unimore.it>
2022-09-12 18:14:44 +0200

    finished 1st version of file logging metrics

--- MA_QLearning.py
+++ MA_QLearning.py
@@ -36,19 +36,19 @@ with open(OUTPUT_FILE, 'w') as f:
     f.write(f"decay = {decay}\n")
     f.write("----------\n")
     # From NetlogoDataAnalysis: Episode, Tick, Avg cluster size X tick, Avg reward X episode, move-toward-chemical, random-walk, drop-chemical, (learner 0)-move-toward-chemical
-    f.write(f"Episodes, Avg cluster size X tick, Avg reward X episode, ")
+    f.write(f"Episode, Tick, Avg cluster size X tick, ")
     for a in l_params["actions"]:
         f.write(f"{a}, ")
-    for l in range(params['population'], params['population']+params['learner_population']-1):
+    for l in range(params['population'], params['population']+params['learner_population']):
         for a in l_params["actions"]:
             f.write(f"(learner {l})-{a}, ")
-    for a in l_params["actions"][:-1]:
-        f.write(f"(learner {params['population']+params['learner_population']-1})-{a}, ")
-    f.write(f"(learner {params['population'] + params['learner_population'] - 1})-{l_params['actions'][-1]}\n")
+    f.write("Avg reward X episode\n")
 
 # Q_table
 qtable = {i: np.zeros([4, 3]) for i in range(params['population'], params['population'] + params['learner_population'])}
 
+# DOC dict che tiene conto della frequenza di scelta delle action per ogni episodio {episode: {action: _, action: _, ...}}
+actions_dict = {str(ep): {str(ac): 0 for ac in range(3)} for ep in range(1, TRAIN_EPISODES+1)}  # DOC 0 = walk, 1 = lay_pheromone, 2 = follow_pheromone
 # DOC dict che tiene conto della frequenza di scelta delle action di ogni agent per ogni episodio {episode: {agent: {action: _, action: _, ...}}}
 action_dict = {str(ep): {str(ag): {str(ac): 0 for ac in range(3)} for ag in range(params['population'], params['population']+params['learner_population'])} for ep in range(1, TRAIN_EPISODES+1)}
 # DOC dict che tiene conto della reward di ogni agente per ogni episodio {episode: {agent: _}}
@@ -95,25 +95,31 @@ for ep in range(1, TRAIN_EPISODES+1):
 
             old_s[agent] = cur_s
 
+            actions_dict[str(ep)][str(action)] += 1
             action_dict[str(ep)][str(agent)][str(action)] += 1
             reward_dict[str(ep)][str(agent)] += round(reward, 2)
         env.move()
         env._evaporate()
         env._diffuse()
         env.render()
+        #print(json.dumps(action_dict, indent=2))
     epsilon *= decay
+    cluster_dict[str(ep)] = round(env.avg_cluster(), 2)
     if ep % TRAIN_LOG_EVERY == 0:
         print(f"EPISODE: {ep}")
         print(f"\tepsilon: {epsilon}")
         #print(f"\tepisode reward: {reward_episode}")
-        avg_rew = 0
-        for a in reward_dict[str(ep)]:
-            avg_rew += (reward_dict[str(ep)][a] / params['episode_ticks'])
-        avg_rew /= params['learner_population']
+        # From NetlogoDataAnalysis: Episode, Tick, Avg cluster size X tick, move-toward-chemical (2), random-walk (0), drop-chemical (1), (learner 0)-move-toward-chemical, ..., Avg reward X episode
         with open(OUTPUT_FILE, 'a') as f:
-            f.write(f"{ep}, {round(avg_rew, 2)}\n")
-    cluster_dict[str(ep)] = round(env.avg_cluster(), 2)
-print(json.dumps(cluster_dict, indent=2))
+            f.write(f"{ep}, {params['episode_ticks'] * ep}, {cluster_dict[str(ep)]}, {actions_dict[str(ep)]['2']}, {actions_dict[str(ep)]['0']}, {actions_dict[str(ep)]['1']}, ")
+            avg_rew = 0
+            for l in range(params['population'], params['population']+params['learner_population']):
+                avg_rew += (reward_dict[str(ep)][str(l)] / params['episode_ticks'])
+                f.write(f"{action_dict[str(ep)][str(l)]['2']}, {action_dict[str(ep)][str(l)]['0']}, {action_dict[str(ep)][str(l)]['1']}, ")
+            avg_rew /= params['learner_population']
+            f.write(f"{avg_rew}\n")
+
+#print(json.dumps(cluster_dict, indent=2))
 print("Training finished!\n")
 
 # DOC Evaluate agent's performance after Q-learning
