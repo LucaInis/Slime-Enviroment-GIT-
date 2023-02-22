ece66adc1af85c64a9cb1a2aa111541cb1170c76
vamsi.anumula-a <vamsi.anumula@rakuten.com>
2023-02-22 13:57:57 +0900

    SB3 compatible version

--- env-test-gym.py
+++ env-test-gym.py
@@ -2,13 +2,13 @@ from stable_baselines3.common.env_checker import check_env
 import slime_environments
 import gym
 import json
+from gym.spaces import MultiBinary
 
 PARAMS_FILE = "slime_environments/agents/single-agent-params.json"
 with open(PARAMS_FILE) as f:
     params = json.load(f)
+
+# print(gym.__version__)
 env = gym.make("Slime-v0", **params)
-# env = Slime(**params)
-# check_env(env)
-from gym.spaces import Box, Dict, Discrete, MultiBinary, MultiDiscrete
-obs = MultiBinary(2)
-print(obs.sample())
+check_env(env)
+print("Environment compatible with Stable Baselines3")
