94963ef6a364118d2e2a37feeaa509b673428aa0
Vamsi Anumula <himavamsianumula@gmail.com>
2023-03-15 11:09:40 +0900

    Test training with SB3

--- env-test-gym.py
+++ env-test-gym.py
@@ -1,8 +1,10 @@
 from stable_baselines3.common.env_checker import check_env
+from stable_baselines3 import DQN
 import slime_environments
 import gym
 import json
 from gym.spaces import MultiBinary
+import numpy as np
 
 PARAMS_FILE = "slime_environments/agents/single-agent-params.json"
 with open(PARAMS_FILE) as f:
@@ -12,3 +14,7 @@ with open(PARAMS_FILE) as f:
 env = gym.make("Slime-v0", **params)
 check_env(env)
 print("Environment compatible with Stable Baselines3")
+
+model = DQN("MlpPolicy", env, verbose=1)
+model.learn(total_timesteps=1000,log_interval=4)
+print("SB3 DQN sample training completed.")
